f7415c536490f08df23d827a083035a4fdf3c690
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-02-23 10:29:41 +0000

    Rename `some_role` to `inhours_primary`
    The 'Person' class now knows which roles are in-hours and which are
    on-call. It's a shame those are now coupled, but we can look to
    unpick it later. In the meantime, keeping things 'generic' was
    making the generator more complicated than it needs to be.

--- spec/rota_generator_spec.rb
+++ spec/rota_generator_spec.rb
@@ -5,38 +5,38 @@ RSpec.describe RotaGenerator do
   let(:fixture_path) { "#{File.dirname(__FILE__)}/fixtures/" }
   let(:roles_config) do
     {
-      some_role: {},
+      inhours_primary: {},
     }
   end
 
   it "raises an exception if a slot can't be filled" do
     people = [
-      Person.new(email: "Dev Eloper", team: "Foo", can_do_roles: [:some_role], forbidden_weeks: [1]),
+      Person.new(email: "Dev Eloper", team: "Foo", can_do_roles: [:inhours_primary], forbidden_in_hours_days: ["01/04/2024"]),
     ]
     slots_to_fill = described_class.new.slots_to_fill(1, roles_config)
 
     expect { described_class.new.fill_slots(people, slots_to_fill) }
-      .to output("WARNING: nobody is available for some_role in week 1\n").to_stdout
+      .to output("WARNING: nobody is available for inhours_primary in week 1\n").to_stdout
   end
 
   it "avoids allocating forbidden_weeks" do
     people = [
-      Person.new(email: "Busy Person",       team: "Foo", can_do_roles: [:some_role], forbidden_weeks: [1, 3]),
-      Person.new(email: "2nd Line Champion", team: "Bar", can_do_roles: [:some_role], forbidden_weeks: []),
+      Person.new(email: "Busy Person",       team: "Foo", can_do_roles: [:inhours_primary], forbidden_weeks: [1, 3]),
+      Person.new(email: "2nd Line Champion", team: "Bar", can_do_roles: [:inhours_primary], forbidden_weeks: []),
     ]
     slots_to_fill = described_class.new.slots_to_fill(3, roles_config)
 
     expect(described_class.new.fill_slots(people, slots_to_fill)).to eq([
-      { week: 1, role: :some_role, assignee: "2nd Line Champion" },
-      { week: 2, role: :some_role, assignee: "Busy Person" },
-      { week: 3, role: :some_role, assignee: "2nd Line Champion" },
+      { week: 1, role: :inhours_primary, assignee: "2nd Line Champion" },
+      { week: 2, role: :inhours_primary, assignee: "Busy Person" },
+      { week: 3, role: :inhours_primary, assignee: "2nd Line Champion" },
     ])
   end
 
   it "spreads shift assignment evenly" do
-    developer_a = Person.new(email: "Developer A", team: "Foo", can_do_roles: [:some_role], forbidden_weeks: [])
-    developer_b = Person.new(email: "Developer B", team: "Bar", can_do_roles: [:some_role], forbidden_weeks: [])
-    developer_c = Person.new(email: "Developer C", team: "Baz", can_do_roles: [:some_role], forbidden_weeks: [])
+    developer_a = Person.new(email: "Developer A", team: "Foo", can_do_roles: [:inhours_primary], forbidden_weeks: [])
+    developer_b = Person.new(email: "Developer B", team: "Bar", can_do_roles: [:inhours_primary], forbidden_weeks: [])
+    developer_c = Person.new(email: "Developer C", team: "Baz", can_do_roles: [:inhours_primary], forbidden_weeks: [])
     people = [developer_a, developer_b, developer_c]
     slots_to_fill = described_class.new.slots_to_fill(9, roles_config)
 
@@ -48,11 +48,11 @@ RSpec.describe RotaGenerator do
   end
 
   it "doesn't assign the same person to simultaneous shifts nor to shifts they can't do" do
-    developer_a = Person.new(email: "Developer A", team: "Foo", can_do_roles: %i[some_role some_other_role], forbidden_weeks: [])
-    developer_b = Person.new(email: "Developer B", team: "Bar", can_do_roles: [:some_role], forbidden_weeks: [])
+    developer_a = Person.new(email: "Developer A", team: "Foo", can_do_roles: %i[inhours_primary some_other_role], forbidden_weeks: [])
+    developer_b = Person.new(email: "Developer B", team: "Bar", can_do_roles: [:inhours_primary], forbidden_weeks: [])
     people = [developer_a, developer_b]
     roles_config = {
-      some_role: {},
+      inhours_primary: {},
       some_other_role: {},
     }
     slots_to_fill = described_class.new.slots_to_fill(3, roles_config)
@@ -62,7 +62,7 @@ RSpec.describe RotaGenerator do
     expect(developer_a.assigned_shifts.count).to eq(3)
     expect(developer_a.assigned_shifts.map { |shift| shift[:role] }.uniq).to eq([:some_other_role])
     expect(developer_b.assigned_shifts.count).to eq(3)
-    expect(developer_b.assigned_shifts.map { |shift| shift[:role] }.uniq).to eq([:some_role])
+    expect(developer_b.assigned_shifts.map { |shift| shift[:role] }.uniq).to eq([:inhours_primary])
   end
 
   it "can handle real datasets" do
