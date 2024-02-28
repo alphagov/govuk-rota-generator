58ce14835764f14582411068d36e08faf5b2191f
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-02-28 13:15:57 +0000

    Add 'DataProcessor' class
    The intention is for the DataProcessor class to be the only place
    we need to update whenever there is a change in the structure of
    the upstream data (Technical Support sheet and Availability /
    Responses sheet). Over the next few commits, we'll delete a lot
    of code from elsewhere that is made redundant as a result.
    
    Note that the `combine_raw` method is not intended to be called
    from upstream: it is only exposed for testing. In the next commit
    we'll write the `combine` method that takes CSV file inputs and
    outputs a YML file, and uses `combine_raw` under the hood.

---
+++ lib/data_processor.rb
@@ -0,0 +1,94 @@
+require "date"
+require_relative "./person"
+
+class InvalidStructureException < StandardError; end
+
+class DataProcessor
+  def self.combine_raw(people_data, responses_data)
+    people = []
+    people_data.each do |person_data|
+      response_data = responses_data.find { |response| person_data["Email"] == response["Email address"] }
+      week_commencing_fields = responses_data.headers.select { |header| header.match(/^Week commencing/) }
+
+      email = response_data["Email address"]
+      team = response_data["What team/area are you in (or will be in when this rota starts)?"]
+      non_working_days = non_working_days(response_data)
+      forbidden_in_hours_days = forbidden_in_hours_days(week_commencing_fields, response_data)
+      forbidden_on_call_days = forbidden_on_call_days(week_commencing_fields, response_data)
+      can_do_roles = [
+        person_data["Eligible for in-hours primary?"] == "Yes" ? :inhours_primary : nil,
+        person_data["Can do in-hours secondary?"] == "Yes" ? :inhours_secondary : nil,
+        person_data["Eligible for in-hours primary?"] == "Yes" ? :inhours_primary_standby : nil,
+        person_data["Can do in-hours secondary?"] == "Yes" ? :inhours_secondary_standby : nil,
+        person_data["Should be scheduled for on-call?"] == "Yes" && person_data["Eligible for on-call primary?"] == "Yes" ? :oncall_primary : nil,
+        person_data["Should be scheduled for on-call?"] == "Yes" && person_data["Eligible for on-call secondary?"] == "Yes" ? :oncall_secondary : nil,
+      ].compact
+
+      people << Person.new(
+        email:,
+        team:,
+        non_working_days:,
+        forbidden_in_hours_days:,
+        forbidden_on_call_days:,
+        can_do_roles:,
+      )
+    end
+    people
+  end
+
+  def self.non_working_days(response_data)
+    non_working_days = response_data["Do you have any non working days? [Non working day(s)]"].split(",")
+    non_working_days = [] if non_working_days == [""]
+    non_working_days.map { |abbreviated_day|
+      return nil if abbreviated_day == ""
+
+      Date.parse(abbreviated_day).strftime("%A")
+    }.compact
+  end
+
+  def self.forbidden_in_hours_days(week_commencing_fields, response)
+    forbidden_dates = []
+    week_commencing_fields.each do |week_commencing_field|
+      next unless (person_response = response[week_commencing_field])
+
+      if availability_strings(person_response).include?("Not available for in-hours")
+        forbidden_dates += weekdays(week_commencing_field)
+      end
+    end
+    forbidden_dates
+  end
+
+  def self.forbidden_on_call_days(week_commencing_fields, response)
+    forbidden_dates = []
+    week_commencing_fields.each do |week_commencing_field|
+      next unless (person_response = response[week_commencing_field])
+
+      if availability_strings(person_response).include?("Not available for on-call weekday nights")
+        forbidden_dates += weekdays(week_commencing_field)
+      end
+
+      if availability_strings(person_response).include?("Not available for on-call over the weekend")
+        forbidden_dates += weekends(week_commencing_field)
+      end
+    end
+    forbidden_dates
+  end
+
+  def self.availability_strings(person_response)
+    person_response.split(",").map(&:strip)
+  end
+
+  def self.weekdays(week_commencing_field)
+    (0..4).to_a.map do |n|
+      first_unavailable_date = week_commencing_field.match(/^Week commencing (.+)$/)[1]
+      (Date.parse(first_unavailable_date) + n).strftime("%d/%m/%Y")
+    end
+  end
+
+  def self.weekends(week_commencing_field)
+    monday = week_commencing_field.match(/^Week commencing (.+)$/)[1]
+    saturday = (Date.parse(monday) + 5).strftime("%d/%m/%Y")
+    sunday = (Date.parse(monday) + 6).strftime("%d/%m/%Y")
+    [saturday, sunday]
+  end
+end
