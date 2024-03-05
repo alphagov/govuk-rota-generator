24229823358a137e5d0f9b7a053ca0468ccb591e
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-03-05 12:03:53 +0000

    Implement Rota Generator v2
    Putting all the recent changes together into `generate_rota.rb`:
    
    - A rota is now generated in terms of individual dates rather than
      weeks. This is much more flexible, allowing us to 1) account for
      non-working days, 2) account for partial unavailability by role
      type (e.g. someone may be eligible for on-call during the week
      but unavailable at the weekend).
    - The generated rota is stored locally as YML so that it can be
      easily re-uploaded to Google Sheets, or indeed later plugged
      into PagerDuty.
    - The generated rota is also automatically synced to Google Sheets
      on the same spreadsheet that the responses are stored in,
      giving 2nd line leads a quick way of evaluating draft rotas.
    - The README describes how we can run the fairness checker against
      an arbitrary rota. I decided not to include this in the default
      output of `bin/generate_rota.rb` as it got quite noisy.

--- bin/generate_rota.rb
+++ bin/generate_rota.rb
@@ -1,1 +1,31 @@
-puts "TODO"
+require "yaml"
+require_relative "../lib/algorithms/weekly"
+require_relative "../lib/google_sheet"
+require_relative "../lib/person"
+require_relative "../lib/rota_presenter"
+
+yml = YAML.load_file(File.dirname(__FILE__) + "/../data/rota_inputs.yml", symbolize_names: true)
+people = yml[:people].map { |person_data| Person.new(**person_data) }
+dates = yml[:dates]
+roles_config = YAML.load_file("#{File.dirname(__FILE__)}/../config/roles.yml", symbolize_names: true)
+
+Algorithms::Weekly.fill_slots!(dates:, people:, roles_config:)
+
+presenter = RotaPresenter.new(dates:, people:, roles_config:)
+
+puts "Writing rota to YML locally..."
+File.write("#{File.dirname(__FILE__)}/../data/generated_rota.yml", presenter.to_yaml)
+
+SHEET_URL = ARGV.first
+if SHEET_URL.nil?
+  puts "All shifts allocated. See CSV below:"
+  puts ""
+  puts presenter.to_csv_weekly
+  puts ""
+  puts "You can automatically write this output to Google Sheet by providing a Google Sheet URL as a CLI arg."
+else
+  puts "Writing CSV to Google Sheets"
+  SHEET_ID = SHEET_URL.match(/spreadsheets\/d\/([^\/]+)/)[1]
+  GoogleSheet.new(scope: :write).write(sheet_id: SHEET_ID, csv: presenter.to_csv_weekly)
+  puts "Draft rota visible in the relevant worksheet at https://docs.google.com/spreadsheets/d/#{SHEET_ID}/edit"
+end
