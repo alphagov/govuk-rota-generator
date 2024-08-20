1eecf6e4c350cd243ca2c36a5894d43fedb83176
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-08-20 11:58:08 +0100

    Auto-correct remaining rubocop offences

--- spec/google_sheet_spec.rb
+++ spec/google_sheet_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GoogleSheet do
 
   let(:sheet_id) { "1sK8ktAnffewnfiewnfoewifnwoein" }
   let(:mock_sheets_api) do
-    mock_sheets_api = instance_double("Google::Apis::SheetsV4::SheetsService")
+    mock_sheets_api = instance_double(Google::Apis::SheetsV4::SheetsService)
     allow(mock_sheets_api).to receive(:authorization=)
     mock_sheets_api
   end
@@ -17,7 +17,7 @@ RSpec.describe GoogleSheet do
   describe "#fetch" do
     it "returns data if sheet is found" do
       range = "SomeWorksheet!A1:Z"
-      mock_response = instance_double("Google::Apis::SheetsV4::ValueRange", values: %w[foo])
+      mock_response = instance_double(Google::Apis::SheetsV4::ValueRange, values: %w[foo])
       allow(mock_sheets_api).to receive(:get_spreadsheet_values).with(sheet_id, range).and_return(mock_response)
 
       result = described_class.new(sheets_api: mock_sheets_api)
@@ -28,7 +28,7 @@ RSpec.describe GoogleSheet do
 
     it "defaults to range 'Form responses 1!A1:Z' (the default worksheet name in spreadsheets linked to Forms)" do
       allow(mock_sheets_api).to receive(:get_spreadsheet_values).and_return(
-        instance_double("Google::Apis::SheetsV4::ValueRange", values: %w[foo]),
+        instance_double(Google::Apis::SheetsV4::ValueRange, values: %w[foo]),
       )
       expect(mock_sheets_api).to receive(:get_spreadsheet_values).with(sheet_id, "Form responses 1!A1:Z")
 
@@ -36,7 +36,7 @@ RSpec.describe GoogleSheet do
     end
 
     it "saves the data as a CSV if filepath provided" do
-      mock_value_range = instance_double("Google::Apis::SheetsV4::ValueRange", values:
+      mock_value_range = instance_double(Google::Apis::SheetsV4::ValueRange, values:
         [
           %w[foo bar],
           %w[baz bash],
