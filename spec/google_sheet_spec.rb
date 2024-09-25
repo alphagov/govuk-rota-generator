96fd4ca63243f86f5d9c28c566d13a2cd7f5dd62
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-09-25 07:44:18 +0100

    Allow google_service_account_key to be passed via ENV var
    This is another pre-requisite for running the PagerDuty sync
    script on GitHub Actions.

--- spec/google_sheet_spec.rb
+++ spec/google_sheet_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe GoogleSheet do
   before do
     mock_authorizer = double("Google Authorizer", fetch_access_token!: true) # rubocop:disable RSpec/VerifiedDoubles
     allow(Google::Auth::ServiceAccountCredentials).to receive(:make_creds).and_return(mock_authorizer)
-    allow(File).to receive(:open).with("./google_service_account_key.json").and_return("{}")
   end
 
   let(:sheet_id) { "1sK8ktAnffewnfiewnfoewifnwoein" }
@@ -14,7 +13,30 @@ RSpec.describe GoogleSheet do
     mock_sheets_api
   end
 
+  def stub_google_service_account_key_file
+    allow(File).to receive(:file?).with("./google_service_account_key.json").and_return(true)
+    allow(File).to receive(:open).with("./google_service_account_key.json").and_return("{}")
+  end
+
+  describe "initialising class with google service account key" do
+    it "uses ENV var if that exists" do
+      stub_const("GoogleSheet::GOOGLE_SERVICE_ACCOUNT_KEY", "foo")
+      expect { described_class.new(sheets_api: mock_sheets_api) }.not_to raise_exception
+    end
+
+    it "uses file if no ENV var exists" do
+      stub_google_service_account_key_file
+      expect { described_class.new(sheets_api: mock_sheets_api) }.not_to raise_exception
+    end
+
+    it "errors if no ENV var and no file exist" do
+      expect { described_class.new(sheets_api: mock_sheets_api) }.to raise_exception(GoogleSheetException)
+    end
+  end
+
   describe "#fetch" do
+    before { stub_google_service_account_key_file }
+
     it "returns data if sheet is found" do
       range = "SomeWorksheet!A1:Z"
       mock_response = instance_double(Google::Apis::SheetsV4::ValueRange, values: %w[foo])
@@ -56,6 +78,8 @@ RSpec.describe GoogleSheet do
   end
 
   describe "#write" do
+    before { stub_google_service_account_key_file }
+
     let(:range) { "SomeWorksheet!A1:Z" }
 
     it "clears the sheet before writing values" do
